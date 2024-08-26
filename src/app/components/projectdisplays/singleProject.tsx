296dbbad87ea38e643fa89e88b535cb78687da39
JohannesJ93 <142202508+JohannesJ93@users.noreply.github.com>
2024-08-26 09:07:25 +0300

    Update singleProject.tsx

--- src/app/components/projectdisplays/singleProject.tsx
+++ src/app/components/projectdisplays/singleProject.tsx
@@ -1,12 +1,14 @@
 
 'use client'
 import { useState } from 'react'
+import Image from 'next/image';
 
 const product = {
   name: 'Code generator for multiple programming languages',
   description:
     'Generating code is essential because it significantly boosts productivity, allowing developers to automate repetitive tasks and focus on more complex aspects of a project. It ensures consistency across the codebase, reducing the likelihood of errors and making the code easier to maintain. By standardizing code structures and patterns, it also facilitates collaboration among team members. Additionally, code generation adapts quickly to changes, allowing for rapid updates across the entire project, saving time and effort. Overall, it streamlines development, improves code quality, and accelerates the delivery of reliable software.',
   imageSrc: '/assets/generateit.png',
+  altSrc: 'secret',
 }
 
 export default function SingleProject() {
@@ -28,7 +30,7 @@ export default function SingleProject() {
                 <h2 className="projectsh4 text-8xl font-bold text-white ">Top Secret</h2>
               </div>
             ) : (
-              <Image src={product.imageSrc} className="h-full w-full object-cover  blur" />
+              < src={product.imageSrc} alt={product.altSrc} className="h-full w-full object-cover  blur" />
             )}
           </div>
 
