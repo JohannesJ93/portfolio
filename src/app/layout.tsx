ff4656fb9dcd72dbedc0c208ca1fc3f4b722153d
Johannes Jokinen <johannes.jokinen_93@hotmail.com>
2025-02-25 15:24:21 +0200

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ClientOnlyThemeProvider from '../ClientOnlyThemeProvider'; // Adjust the 
 import Link from 'next/link';
 import NavBar from "./components/navbars/navbar";
 import TestPage from "./test/page";
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientOnlyThemeProvider>
+          <Analytics/>
           <TestPage/>
           <NavBar />
           {children}
